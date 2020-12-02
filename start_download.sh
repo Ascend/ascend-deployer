84387d1410a4484e79b0af7e85d7acdeb66c44c0
yuncliu <liuyuncheng@huawei.com>
2020-12-02 21:31:00 +0800

    执行下载前检查是否有python3，没有则安装 修复fact设置

--- start_download.sh
+++ start_download.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+have_python3=`command -v python3 | wc -l`
+have_yum=`command -v python3 | wc -l`
+have_apt=`command -v apt | wc -l`
+if [ ${have_python3} -eq 0 ];then
+    if [ ${have_yum} -eq 1 ];then
+        yum install -y python3
+    fi
+    if [ ${have_apt} -eq 1 ];then
+        DEBIAN_FRONTEND=noninteractive apt -y install python3
+    fi
+fi
 python3 downloader/downloader.py
 python3 downloader/other_downloader.py
