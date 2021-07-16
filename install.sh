ac696f6c7ac1a03781a1bce05b2ecf9efc5364e1
funnyfunny <1360681597@qq.com>
2021-07-16 09:32:14 +0800

    修复bclinux bug

--- install.sh
+++ install.sh
@@ -251,7 +251,7 @@ function install_sys_packages()
     install_kernel_header_devel_euler
     local have_rpm=0
     case ${g_os_name} in
-    CentOS|EulerOS|SLES|Kylin|BCLinux|Tlinux)
+    CentOS|EulerOS|SLES|Kylin|BCLinux|Tlinux|OpenEuler)
         local have_rpm=1
         ;;
     Ubuntu|Debian|Linx|UOS)
@@ -755,7 +755,10 @@ function process_chean()
 
 function bootstrap()
 {
-    local have_ansible=`command -v ansible | wc -l`
+    export PATH=${PYTHON_PREFIX}/bin:$PATH
+    export LD_LIBRARY_PATH=${PYTHON_PREFIX}/lib:$LD_LIBRARY_PATH
+    unset PYTHONPATH
+
     check_python375
     local py37_status=$?
     if [ ${py37_status} == ${FALSE} ];then
@@ -763,8 +766,10 @@ function bootstrap()
         install_python375
     fi
 
-    if [ ${have_ansible} -eq 0 ];then
-        echo "no ansible"
+    local have_ansible_cmd=$(command -v ansible | wc -l)
+    have_no_python_module "ansible"
+    if [[ $? == ${TRUE} ]] || [[ ${have_ansible_cmd} == 0 ]];then
+        log_warning "no ansible"
         install_ansible
     fi
 }
