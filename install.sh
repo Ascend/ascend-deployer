c7bce53e45cd2e1020f8a109b3ad018c451c5346
刘勇 <1390871916@qq.com>
2022-02-22 14:41:41 +0800

    升级ansible版本

--- install.sh
+++ install.sh
@@ -484,33 +484,33 @@ function install_ansible()
     local ansible_path=${PYTHON_PREFIX}/lib/${PYTHON_MINOR}/site-packages/ansible
     ${PYTHON_MINOR} -m ensurepip
     ${PYTHON_MINOR} -m pip install --upgrade pip --no-index --find-links ${PYLIB_PATH}
-    ${PYTHON_MINOR} -m pip install ansible --no-index --find-links ${PYLIB_PATH}
+    ${PYTHON_MINOR} -m pip install ansible-core --no-index --find-links ${PYLIB_PATH}
     # patch the INTERPRETER_PYTHON_DISTRO_MAP, make it support EulerOS
     if [ -f ${ansible_path}/config/base.yml ];then
         eulercnt=$(grep euleros ${ansible_path}/config/base.yml | wc -l)
         if [ ${eulercnt} == 0 ];then
             # euler os 2 is recoginized as centos 2
-            sed -i "1515 i\      '2': /usr/bin/python3"     ${ansible_path}/config/base.yml
+            sed -i "1501 i\      '2': /usr/bin/python3"     ${ansible_path}/config/base.yml
             # ubuntu 18.04 is recoginized as debian buster/sid due tu /etc/debian_release
-            sed -i "1520 i\      'buster/sid': /usr/bin/python3" ${ansible_path}/config/base.yml
+            sed -i "1506 i\      'buster/sid': /usr/bin/python3" ${ansible_path}/config/base.yml
             # euler os use python3 as default python interpreter
-            sed -i "1528 i\    euleros:"                    ${ansible_path}/config/base.yml
-            sed -i "1529 i\      '2': /usr/bin/python3"     ${ansible_path}/config/base.yml
+            sed -i "1516 i\    euleros:"                    ${ansible_path}/config/base.yml
+            sed -i "1517 i\      '2': /usr/bin/python3"     ${ansible_path}/config/base.yml
             # kylin should use python3. if selinux enalbed, the default python have no selinux
-            sed -i "1530 i\    kylin:"                      ${ansible_path}/config/base.yml
-            sed -i "1531 i\      '10': /usr/bin/python3"    ${ansible_path}/config/base.yml
-            sed -i "1532 i\      'V10': /usr/bin/python3"    ${ansible_path}/config/base.yml
+            sed -i "1518 i\    kylin:"                      ${ansible_path}/config/base.yml
+            sed -i "1519 i\      '10': /usr/bin/python3"    ${ansible_path}/config/base.yml
+            sed -i "1520 i\      'V10': /usr/bin/python3"    ${ansible_path}/config/base.yml
             # debian 10.0
-            sed -i "1520 i\      '10.0': /usr/bin/python3" ${ansible_path}/config/base.yml
+            sed -i "1506 i\      '10.0': /usr/bin/python3" ${ansible_path}/config/base.yml
             # ubuntu 20.04 is recoginized as debian bullseye/sid due to /etc/debian_version
-            sed -i "1522 i\      'bullseye/sid': /usr/bin/python3" ${ansible_path}/config/base.yml
+            sed -i "1508 i\      'bullseye/sid': /usr/bin/python3" ${ansible_path}/config/base.yml
             # openeuler os use python3 as default python interpreter
-            sed -i "1535 i\    openeuler:"                    ${ansible_path}/config/base.yml
-            sed -i "1536 i\      '20.03': /usr/bin/python3"     ${ansible_path}/config/base.yml
-            sed -i "1537 i\    uos:"                    ${ansible_path}/config/base.yml
-            sed -i "1538 i\      '20': /usr/bin/python3"     ${ansible_path}/config/base.yml
-            sed -i "1539 i\    uniontech:"                    ${ansible_path}/config/base.yml
-            sed -i "1540 i\      '20': /usr/bin/python3"     ${ansible_path}/config/base.yml
+            sed -i "1523 i\    openeuler:"                    ${ansible_path}/config/base.yml
+            sed -i "1524 i\      '20.03': /usr/bin/python3"     ${ansible_path}/config/base.yml
+            sed -i "1525 i\    uos:"                    ${ansible_path}/config/base.yml
+            sed -i "1526 i\      '20': /usr/bin/python3"     ${ansible_path}/config/base.yml
+            sed -i "1527 i\    uniontech:"                    ${ansible_path}/config/base.yml
+            sed -i "1528 i\      '20': /usr/bin/python3"     ${ansible_path}/config/base.yml
         fi
     fi
 }
