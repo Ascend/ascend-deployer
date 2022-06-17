9b50ff972839ffa7331f3e42bd6878919634ae11
liuyong <1390871916@qq.com>
2022-06-17 16:09:24 +0800

    兼容驱动旧名

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ readonly arch=$(uname -m)
 readonly BASE_DIR=$(cd "$(dirname $0)" > /dev/null 2>&1; pwd -P)
 readonly PYLIB_PATH=${BASE_DIR}/resources/pylibs
 readonly A310P_PRODUCT_LIST="Ascend310P"
+readonly A300I_PRODUCT_LIST="A300i-pro,Atlas-300i-pro"
+readonly A300V_PRODUCT_LIST="A300v-pro,Atlas-300v-pro"
+readonly A300IDUO_PRODOUCT_LIST="Atlas-300i-duo,A300i-duo"
 readonly INFER_PRODUCT_LIST="Ascend310,A300-3000,A300-3010,Atlas-200"
 readonly TRAIN_PRODUCT_LIST="Ascend910,A300t-9000,A800-9000,A800-9010,A900-9000"
 readonly CANN_PRODUCT_LIST="Ascend-cann,Ascend-mindx"
@@ -718,6 +721,12 @@ function zip_extract()
             local run_from_zip=${BASE_DIR}/resources/run_from_cann_zip
         elif [[ $(check_npu_scene ${A310P_PRODUCT_LIST} $(basename ${zip_file}))  == 1 ]];then
             local run_from_zip=${BASE_DIR}/resources/run_from_a310p_zip
+        elif [[ $(check_npu_scene ${A300I_PRODUCT_LIST} $(basename ${zip_file}))  == 1 ]];then
+            local run_from_zip=${BASE_DIR}/resources/run_from_a300i_zip
+        elif [[ $(check_npu_scene ${A300V_PRODUCT_LIST} $(basename ${zip_file}))  == 1 ]];then
+            local run_from_zip=${BASE_DIR}/resources/run_from_a300v_zip
+        elif [[ $(check_npu_scene ${A300IDUO_PRODOUCT_LIST} $(basename ${zip_file}))  == 1 ]];then
+            local run_from_zip=${BASE_DIR}/resources/run_from_a300iduo_zip
         elif [[ $(check_npu_scene ${INFER_PRODUCT_LIST} $(basename ${zip_file}))  == 1 ]];then
             local run_from_zip=${BASE_DIR}/resources/run_from_infer_zip
         elif [[ $(check_npu_scene ${TRAIN_PRODUCT_LIST} $(basename ${zip_file}))  == 1 ]];then
