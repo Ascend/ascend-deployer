d316118d0a4071e6849851f141777e31ceb62709
swagglian <1312140415@qq.com>
2023-03-04 06:54:37 +0000

    add offline-deploy/scripts/image_load.sh.
    Signed-off-by: swagglian <1312140415@qq.com>

---
+++ offline-deploy/scripts/image_load.sh
@@ -0,0 +1,6 @@
+current_dir=$(cd $(dirname $0); pwd)
+inventory_file_dir=$(cd $current_dir; pwd)
+inventory_file_path="$inventory_file_dir/inventory_file"
+yamls_dir=$(cd $current_dir/..; pwd)
+
+ansible-playbook -i $inventory_file_path $yamls_dir/yamls/image_load.yaml -vv
