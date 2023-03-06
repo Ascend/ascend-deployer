1dc02ce9541444709a6e1d39556bd2cfdf5dd168
swagglian <1312140415@qq.com>
2023-03-06 07:31:55 +0000

    update offline-deploy/scripts/image_load.sh.
    Signed-off-by: swagglian <1312140415@qq.com>

--- offline-deploy/scripts/image_load.sh
+++ offline-deploy/scripts/image_load.sh
@@ -1,6 +1,15 @@
 current_dir=$(cd $(dirname $0); pwd)
 inventory_file_dir=$(cd $current_dir; pwd)
-inventory_file_path="$inventory_file_dir/inventory_file"
+inventory_file_path="$inventory_file_dir/../inventory_file"
 yamls_dir=$(cd $current_dir/..; pwd)
+image_file=$1
+if [ ! -f "$image_file" ]; then
+    echo "image file :$image_file does not exist"
+    exit 1
+fi
+if [[ "$image_file" != /* ]]; then
+   echo "image file path should be absolute path"
+       exit 1
+fi
 
-ansible-playbook -i $inventory_file_path $yamls_dir/yamls/image_load.yaml -vv
+ansible-playbook -i $inventory_file_path --extra-vars "image_file=$image_file" $yamls_dir/yamls/image_load.yaml -vv
