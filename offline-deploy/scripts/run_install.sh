02effec649c0b6a44d4d3f1c2e593c17350c65b8
sunchuhan <846577687@qq.com>
2023-03-13 13:49:26 +0800

    5.0.RC1场景下取消enalbeHTTP

--- offline-deploy/scripts/run_install.sh
+++ offline-deploy/scripts/run_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 current_dir=$(cd $(dirname $0); pwd)
-
-bash $current_dir/install_ansible.sh && bash $current_dir/install_npu.sh && bash $current_dir/install.sh
+bash $current_dir/install_ansible.sh && bash $current_dir/install_npu.sh && bash $current_dir/install.sh && bash $current_dir/hccn_set.sh
