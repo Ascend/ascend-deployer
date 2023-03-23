bcdc32eabc62628fcf1bef8e0886504104f1d8c7
swagglian <1312140415@qq.com>
2023-03-23 09:04:03 +0000

    update offline-deploy/scripts/install.sh.
    Signed-off-by: swagglian <1312140415@qq.com>

--- offline-deploy/scripts/install.sh
+++ offline-deploy/scripts/install.sh
@@ -3,8 +3,9 @@ set -e
 current_dir=$(cd $(dirname $0); pwd)
 . $current_dir/utils.sh
 time="$(date +"%Y-%m-%d %H:%M:%S")"
-ansible -i $inventory_file_path all -m shell -a "date -s '$time'; hwclock -w"
 ansible -i $inventory_file_path all -m ping
+
+ansible -i $inventory_file_path all -m shell -a "date -s '$time'; hwclock -w"
 
 ansible-playbook -i $inventory_file_path $inventory_file_dir/yamls/gather_facts.yaml -vv
 
