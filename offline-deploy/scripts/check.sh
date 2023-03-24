4bdffbe44acffd7c9f5e44086493fed061cbf8ba
liuyong <1390871916@qq.com>
2023-03-24 11:43:50 +0800

    增加910b的x86架构安装驱动

--- offline-deploy/scripts/check.sh
+++ offline-deploy/scripts/check.sh
@@ -7,6 +7,6 @@ ansible -i $inventory_file_path all -m ping
 
 ansible-playbook -i $inventory_file_path $inventory_file_dir/yamls/gather_facts.yaml
 
-# ansible-playbook -i $inventory_file_path $inventory_file_dir/yamls/basic.yaml -vv
+ansible-playbook -i $inventory_file_path $inventory_file_dir/yamls/basic.yaml -vv
 
 ansible-playbook -i $inventory_file_path $inventory_file_dir/yamls/check.yaml
